e4482d1187a3d340265ffabec54b23a1eb7f319a
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2021-12-26 19:24:38 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n)
+    const str1 = str.replace(/-/g,'')
+    const arr = Array.from(str1)
+    const rev = arr.reverse()
+    const str2 =rev.join('')
+    const result = Number(str2)
+    return result
 }
